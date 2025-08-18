af2c526f6fbba9b0080f1ae32f16e7817b5aea0c
MustafaaMohamed <mustafa.muhamed9199@gmail.com>
2025-08-19 02:59:58 +0300

    Add UnitOfWork Design Pattern

---
+++ Infrastructure/Persistence/Repositories/UnitOfWork.cs
@@ -0,0 +1,33 @@
+﻿using Domain.Contracts;
+using Domain.Models;
+using Persistence.Data;
+
+namespace Persistence.Repositories
+{
+	public class UnitOfWork : IUnitOfWork
+	{
+		private readonly TalabatDbContext _context;
+		private readonly Dictionary<string, object> _repositories;
+
+		public UnitOfWork(TalabatDbContext context)
+		{
+			_context = context;
+			_repositories = new();
+		}
+		public IGenericRepository<TEntity, TKey> GetRepository<TEntity, TKey>() where TEntity : BaseEntity<TKey>
+		{
+			var type = typeof(TEntity).Name;
+			if (!_repositories.ContainsKey(type))
+			{
+				var repository = new GenericRepository<TEntity, TKey>(_context);
+				_repositories.Add(type, repository);
+			}
+			return (GenericRepository<TEntity, TKey>)_repositories[type] ;
+		}
+
+		public async Task<int> SaveChangesAsync()
+		{
+			return await _context.SaveChangesAsync();
+		}
+	}
+}

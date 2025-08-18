17995010e9b5ee34fcda55193dc38bd1e90755ac
MustafaaMohamed <mustafa.muhamed9199@gmail.com>
2025-08-19 01:10:01 +0300

    Create Db Entities

---
+++ Core/Domain/Models/Product.cs
@@ -0,0 +1,17 @@
+﻿
+namespace Domain.Models
+{
+	public class Product : BaseEntity<int>
+	{
+		public string Name { get; set; }
+		public string Description { get; set; }
+		public string PictureUrl { get; set; }
+
+		public decimal Price { get; set; }
+		public int TypeId { get; set; } // Foreign key
+		public ProductType ProductType { get; set; } // Navigation property
+		public int BrandId { get; set; } // Foreign key
+		public ProductBrand ProductBrand { get; set; } // Navigation property
+
+	}
+}
